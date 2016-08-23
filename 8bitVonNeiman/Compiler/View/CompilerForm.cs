5ffdc500ad33b05453e50ddc3bd18b3d01e5a114
Vasiliy Yatsevich <vy@mpk.mobi>
2016-08-23 17:13:25 +0300

    Созданы файлы для компилятора

---
+++ 8bitVonNeiman/Compiler/View/CompilerForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8bitVonNeiman.Compiler.View {
+    public partial class CompilerForm : Form {
+        public CompilerForm() {
+            InitializeComponent();
+        }
+    }
+}
