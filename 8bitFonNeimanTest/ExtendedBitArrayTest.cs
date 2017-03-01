2d0356d16daa05f5fa32cd7d728f363d5890b273
Vasiliy Yatsevich <vy@mpk.mobi>
2017-03-01 12:16:55 +0300

    Создан собственный класс для массива бит. Покрыт тестами

---
+++ 8bitFonNeimanTest/ExtendedBitArrayTest.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _8bitVonNeiman.Common;
+
+namespace _8bitFonNeimanTest {
+    /// <summary>
+    /// Сводное описание для ExtendedByteArrayTest
+    /// </summary>
+    [TestClass]
+    public class ExtendedBitArrayTest {
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Получает или устанавливает контекст теста, в котором предоставляются
+        ///сведения о текущем тестовом запуске и обеспечивается его функциональность.
+        ///</summary>
+        public TestContext TestContext {
+            get {
+                return testContextInstance;
+            }
+            set {
+                testContextInstance = value;
+            }
+        }
+
+        #region Дополнительные атрибуты тестирования
+        //
+        // При написании тестов можно использовать следующие дополнительные атрибуты:
+        //
+        // ClassInitialize используется для выполнения кода до запуска первого теста в классе
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // ClassCleanup используется для выполнения кода после завершения работы всех тестов в классе
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // TestInitialize используется для выполнения кода перед запуском каждого теста 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // TestCleanup используется для выполнения кода после завершения каждого теста
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void ConstructorTest() {
+            var array = new List<string> { "00001100", "01110111", "00011100", "01010101", "01010001" };
+            foreach (var code in array) {
+                var bitArray = new ExtendedBitArray(code);
+                Assert.AreEqual(bitArray.ToDigitString(), code);
+            }
+        }
+
+        [TestMethod]
+        public void EmptyConstructorTest() {
+            var bitArray = new ExtendedBitArray();
+            Assert.AreEqual(bitArray.ToDigitString(), "00000000");
+        }
+
+        [TestMethod]
+        public void CopyConstructorTest() {
+            var bitArray = new ExtendedBitArray("00110000");
+            var copy = new ExtendedBitArray(bitArray);
+            Assert.AreEqual(bitArray.ToDigitString(), copy.ToDigitString());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void LongConstructorExceptionTest() {
+            var bitArray = new ExtendedBitArray("110011100");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void WrongFormatConstructorExceptionTest() {
+            var bitArray = new ExtendedBitArray("11201100");
+        }
+
+        [TestMethod]
+        public void AddMethodTest() {
+            var array1 = new List<ExtendedBitArray> {
+                new ExtendedBitArray("00001100"), //12
+                new ExtendedBitArray("01110111"), //119
+                new ExtendedBitArray("00011100"), //28
+                new ExtendedBitArray("01010001"), //81
+                new ExtendedBitArray("01010101"), //85
+            };
+            var array2 = new List<ExtendedBitArray> {
+                new ExtendedBitArray("00000011"), //3
+                new ExtendedBitArray("00100100"), //36
+                new ExtendedBitArray("11000000"), //192
+                new ExtendedBitArray("00001001"), //9
+                new ExtendedBitArray("00001011"), //11
+            };
+            var results = new List<string> {
+                "00001111", //15
+                "10011011", //155
+                "11011100", //220
+                "01011010", //90
+                "01100000", //96
+            };
+            for (int i = 0; i < array1.Count; i++) {
+                array1[i].Add(array2[i]);
+                Assert.AreEqual(array1[i].ToDigitString(), results[i]);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException))]
+        public void AddOverflowMethodTest() {
+            var array1 = new ExtendedBitArray("11111111");
+            var array2 = new ExtendedBitArray("00000001");
+            array1.Add(array2);
+        }
+
+        [TestMethod]
+        public void IncTest() {
+            var array = new List<ExtendedBitArray> {
+                new ExtendedBitArray("00000011"), //3
+                new ExtendedBitArray("00100100"), //36
+                new ExtendedBitArray("11000000"), //192
+                new ExtendedBitArray("00001001"), //9
+                new ExtendedBitArray("00001011"), //11
+            };
+            var results = new List<string> {
+                "00000100", 
+                "00100101", 
+                "11000001", 
+                "00001010",
+                "00001100", 
+            };
+            for (int i = 0; i < array.Count; i++) {
+                array[i].Inc();
+                Assert.AreEqual(array[i].ToDigitString(), results[i]);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException))]
+        public void IncOverflowExceptionTest() {
+            var bitArray = new ExtendedBitArray("11111111");
+            bitArray.Inc();
+        }
+
+        [TestMethod]
+        public void DecTest() {
+            var array = new List<ExtendedBitArray> {
+                new ExtendedBitArray("00000011"), //3
+                new ExtendedBitArray("00100100"), //36
+                new ExtendedBitArray("11000000"), //192
+                new ExtendedBitArray("00001001"), //9
+                new ExtendedBitArray("00001011"), //11
+            };
+            var results = new List<string> {
+                "00000010",
+                "00100011",
+                "10111111",
+                "00001000",
+                "00001010"
+            };
+            for (int i = 0; i < array.Count; i++) {
+                array[i].Dec();
+                Assert.AreEqual(array[i].ToDigitString(), results[i]);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException))]
+        public void DecOverflowExceptionTest() {
+            var bitArray = new ExtendedBitArray("00000000");
+            bitArray.Dec();
+        }
+
+        [TestMethod]
+        public void GetBitTest() {
+            var code = "00100101";
+            var array = new ExtendedBitArray(code);
+            for (int i = 0; i < 8; i++) {
+                Assert.AreEqual(array[i], code[7 - i] == '1');
+            }
+        }
+
+        [TestMethod]
+        public void SetBitTest() {
+            var array = new ExtendedBitArray("00100101");
+
+            array[1] = true;
+            Assert.AreEqual(array.ToDigitString(), "00100111");
+
+            array[1] = true;
+            Assert.AreEqual(array.ToDigitString(), "00100111");
+
+            array[1] = false;
+            Assert.AreEqual(array.ToDigitString(), "00100101");
+
+            array[7] = false;
+            Assert.AreEqual(array.ToDigitString(), "00100101");
+
+            array[7] = true;
+            Assert.AreEqual(array.ToDigitString(), "10100101");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetBitArgumentExceptionTest() {
+            var array = new ExtendedBitArray();
+            var temp = array[8];
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SetBitArgumentExceptionTest() {
+            var array = new ExtendedBitArray();
+            array[8] = true;
+        }
+    }
+}
---
+++ 8bitVonNeiman/Common/ExtendedBitArray.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace _8bitVonNeiman.Common {
+
+    /// Класс, предоставляющий удобную работу с числом как с массивом байт.
+    public class ExtendedBitArray {
+
+        /// Информация байта. При изменении размера слова нужно будет изменить тип.
+        private byte _data;
+
+        public ExtendedBitArray() {
+            //Do nothing
+        }
+
+        /// <summary>
+        /// Конструктор, принимающий на вход двоичную интерпретацию байта вида 00100111. 
+        /// 
+        /// При некорректном формате аргумента будет сгенерировано исключение.
+        /// </summary>
+        /// <param name="code">Байт в двоичном формате вида 00110110</param>
+        public ExtendedBitArray(string code) {
+            if (code.Length > Constants.WordSize) {
+                throw new ArgumentException("Код байта не может быть больше " + new string('1', Constants.WordSize));
+            }
+            try {
+                _data = (byte)Convert.ToInt32(code, 2);
+            } catch (Exception e) {
+                throw new ArgumentException($"Некорректный код байта ({code})", e);
+            }
+        }
+
+        /// <summary>
+        /// Создает копию передаваемого слова
+        /// </summary>
+        public ExtendedBitArray(ExtendedBitArray array) {
+            _data = array._data;
+        }
+
+        /// <summary>
+        /// Возвращает информацию байта в виде двоичной строки.
+        /// </summary>
+        /// <returns>Информация, хранимая в байте в виде двоичной строки.</returns>
+        public string ToDigitString() {
+            var s = Convert.ToString(_data, 2);
+            var diff = Constants.WordSize - s.Length;
+            if (diff == 0) {
+                return s;
+            } else {
+                return new string('0', diff) + s;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает информацию байта в виде шестнадцатиричной строки.
+        /// </summary>
+        /// <returns>Инфомация, хранимая в байте в виде шестнадцатиричной строки.</returns>
+        public string ToHexString() {
+            return _data.ToString("X2");
+        }
+
+        /// <summary>
+        /// Прибавляет значение передаваемого слова к текущему слову. Генерирует <see cref="OverflowException"/> при переполнении.
+        /// </summary>
+        /// <param name="array">Байт, значение которого прибавляется к текущему.</param>
+        public void Add(ExtendedBitArray array) {
+            checked {
+                _data += array._data;
+            }
+        }
+
+        /// <summary>
+        /// Возвращает или задает значения бита с переданным индексом.
+        /// </summary>
+        /// <param name="key">Индекс бита.</param>
+        /// <returns>Значение бита.</returns>
+        public bool this[int key] {
+            get {
+                if (key >= Constants.WordSize || key < 0) {
+                    throw new ArgumentException("Индекс находится вне границ диапазлна");
+                }
+                return ((_data >> key) & 1) == 1;
+            }
+            set {
+                if (key >= Constants.WordSize || key < 0) {
+                    throw new ArgumentException("Индекс находится вне границ диапазлна");
+                }
+                int mask = 1 << key;
+                if (value) {
+                    _data |= (byte)mask;
+                } else {
+                    _data &= (byte)~mask;
+                }
+            }
+        }
+        
+        /// Увеличивает значение слова на 1. При переполнении вызывает <see cref="OverflowException"/>
+        public void Inc() {
+            checked {
+                _data++;
+            }
+        }
+
+        /// Уменьшает значение слова на 1. При переполнении вызывает <see cref="OverflowException"/>
+        public void Dec() {
+            checked {
+                _data--;
+            }
+        }
+    }
+}
